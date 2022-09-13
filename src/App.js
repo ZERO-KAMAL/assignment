ab69f74c8067e6cddde81a2d790437580313ab66
kamal khatri <kamal1997khatri@gmail.com>
2022-09-13 19:11:28 +0545

    'deploymnet'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { DataGrid } from "@mui/x-data-grid";
+
+// https://www.balldontlie.io/api/v1/teams
 
 function App() {
+  const [data, setData] = useState([]);
+  const getData = async () => {
+    await axios
+      .get("https://www.balldontlie.io/api/v1/teams")
+      .then((res) => {
+        // console.log(res.data);
+        setData(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const columns = [
+    { field: "id", headerName: "ID", width: 150 },
+    { field: "city", headerName: "City", width: 150 },
+    { field: "abbreviation", headerName: "Abbreviation", width: 150 },
+    { field: "conference", headerName: "Conference", width: 150 },
+    { field: "division", headerName: "Division", width: 150 }
+];
+
+  const rows = data.map((row) => ({
+    id: row.id,
+    abbreviation: row.abbreviation,
+    city: row.city,
+    conference: row.conference,
+    division: row.division,
+  }));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ height: 800, width: "100%" }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={10}
+          rowsPerPageOptions={[10]}
+        />
+      </div>
+      {/* {data.map((item, index) => (
+        <div className="" key={index}>
+          {item.title}
+        </div>
+      ))} */}
+    </>
   );
 }
 
